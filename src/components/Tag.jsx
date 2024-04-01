803510fdf5f523f3eb70f44d1f984bd6b43eeee3
Tristan Emmerson <Temmerson@evosus.local>
2024-04-01 09:24:30 -0700

    Refactor widget according to latest requests from product. Changed delete icon to x. Removed edit functionality. Changed delete confirmation to modal confirmation. AddTag btn changed into new tag input field.

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,141 +1,45 @@
 // eslint-disable-next-line no-unused-vars
 import { Component, createElement, useEffect, useRef, useState } from "react";
 
-// export class Tag extends Component {
 export function Tag(props) {
     const input = useRef(null);
-    const [mounted, setMounted] = useState(false);
-    const [editMode, setEditMode] = useState(false);
-    const [confirmDelete, setConfirmDelete] = useState(false);
-    const [tagText, setTagText] = useState(props.tag);
-    const [originalTagText, setOriginalTagText] = useState(props.tag);
-    const [deleted, setDeleted] = useState(false);
-    const handleKeyDown = event => {
-        const keyCode = event.keyCode || event.which;
-        if (keyCode === 13) {
-            event.preventDefault();
-        }
-    };
-
-    // set mounted when render() is first called
-    useEffect(() => {
-        setMounted(true);
-        return () => {};
-    }, []);
-
-    // set span innerHTML to current tagText to mimick <input> behavior
-    useEffect(() => {
-        if (mounted && input.current != null) {
-            // eslint-disable-next-line no-use-before-define
-            setSpanValue(tagText);
-        }
-    }, [mounted, tagText, confirmDelete]);
-
-    // listen for tag changes
-    useEffect(() => {
-        // if this tag was previously deleted it needs to reset
-        // to be re-used with new tagText
-        if (deleted) {
-            setDeleted(false);
-            setConfirmDelete(false);
-        }
-        setTagText(props.tag);
-        setOriginalTagText(props.tag);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.tag]);
-
-    // set span innerHTML
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false);
     const setSpanValue = value => {
         input.current.innerHTML = value;
     };
 
-    // toggle whether the tag is being edited or not
-    const toggleEdit = () => {
-        setEditMode(!editMode);
-    };
+    useEffect(() => {
+        setSpanValue(props.tag);
+    }, [props.tag]);
 
-    // return tag to non-editing state and restore last savged tagText
-    const cancelEdit = () => {
-        setTagText(originalTagText);
-        setSpanValue(originalTagText);
-        setEditMode(false); //now canceling out of edit mode
-    };
-
-    // read value from span element and save changes to state
-    // return to non-editing mode
-    const saveTag = () => {
-        const newTagText = input.current.innerHTML;
-        props.saveTag(props.index, originalTagText, newTagText);
-        setTagText(newTagText);
-        setOriginalTagText(newTagText);
-        setSpanValue(tagText);
-        toggleEdit();
-    };
-
-    // toggle whether the confirm delete message should show or not
-    const toggleConfirmDelete = () => {
-        setConfirmDelete(!confirmDelete);
-    };
-
-    // delete this tag from the master list
     const deleteTag = () => {
-        props.deleteTag(props.index, tagText); //now passing index and tag text arguments
-        setDeleted(true);
+        setShowConfirmDialog(true);
     };
 
-    // if tag is deleted, show nothing
-    // if confirmDelete then show confrim delete prompt
-    // else show the normal tag component
-    return deleted ? "" : confirmDelete ? buildConfirmDeleteTag() : buildTag();
+    const confirmDeleteTag = () => {
+        props.deleteTag(props.index, props.tag);
+        setShowConfirmDialog(false);
+    };
 
-    // build the normal tag component to view or edit
-    function buildTag() {
-        return (
-            <div className="ev-workflow-tag-padding">
-                <div className="ev-workflow-tag-container">
-                    <button
-                        className={"ev-workflow-button ev-workflow-icon ev-workflow-edit"}
-                        onClick={editMode ? saveTag.bind(this) : toggleEdit.bind(this)}
-                    >
-                        <i className={"mdi" + confirmDelete ? (editMode ? " mdi-check" : " mdi-edit") : ""}></i>
-                    </button>
-                    <span
-                        ref={input}
-                        tabIndex="0"
-                        className={"input ev-workflow-input" + (editMode ? " edit-mode" : "")}
-                        role="textbox"
-                        contentEditable={editMode ? "true" : "false"}
-                        onKeyDown={handleKeyDown}
-                    ></span>
-                    <button
-                        className={"ev-workflow-button ev-workflow-icon ev-workflow-delete"}
-                        onClick={editMode ? cancelEdit : toggleConfirmDelete}
-                    >
-                        <i className={editMode ? "mdi mdi-close" : "mdi mdi-delete"}></i>
-                    </button>
-                </div>
-            </div>
-        );
-    }
+    const cancelDeleteTag = () => {
+        setShowConfirmDialog(false);
+    };
 
-    // build tag with confirm delete prompt
-    function buildConfirmDeleteTag() {
-        return (
+    return (
+        <div className="ev-workflow-tag-padding">
             <div className="ev-workflow-tag-container">
-                <button
-                    className={"confirm-delete-button confirm-delete-button-left cancel-delete-button"}
-                    onClick={toggleConfirmDelete}
-                >
-                    Cancel
-                </button>
-                <span className="confirm-delete-text">Are you sure?</span>
-                <button
-                    className={"confirm-delete-button confirm-delete-button-right ev-workflow-delete"}
-                    onClick={deleteTag}
-                >
-                    Delete
+                <span ref={input} tabIndex="0" className="input ev-workflow-input" role="textbox"></span>
+                <button className="ev-workflow-button ev-workflow-icon ev-workflow-delete" onClick={deleteTag}>
+                    <i className="mdi mdi-close"></i>
                 </button>
             </div>
-        );
-    }
+            {showConfirmDialog && (
+                <div className="confirmation-modal" style={{ top: "-275%" }}>
+                    <p>Confirm delete?</p>
+                    <button onClick={cancelDeleteTag}>No</button>
+                    <button onClick={confirmDeleteTag}>Yes</button>
+                </div>
+            )}
+        </div>
+    );
 }
--- src/components/WorkflowContainer.jsx
+++ src/components/WorkflowContainer.jsx
@@ -1,80 +1,65 @@
-// eslint-disable-next-line no-unused-vars
-import { Component, createElement, useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+/* eslint-disable sort-imports */
+/* eslint-disable no-shadow */
+/* eslint-disable no-use-before-define */
+import { Component, createElement, useEffect, useState, useRef } from "react";
 import { Tag } from "./Tag";
 import { v4 as uuidv4 } from "uuid";
-// eslint-disable-next-line sort-imports, no-unused-vars
 import * as mx from "mendix";
 
 export function WorkflowContainer(props) {
     const { onClickMoreAction } = props;
     const { onTagAddAction } = props;
     const [mounted, setMounted] = useState(false);
-    // eslint-disable-next-line no-unused-vars
     const [tagsArray, setTagsArray] = useState([]);
-    // eslint-disable-next-line no-unused-vars
     const [masterTagsList, setMasterTagsList] = useState(props.masterTagsList.value ?? "");
     const [tagComponents, setTagComponents] = useState([]);
-    const [limit] = useState(props.limit ?? 5);
-    const [delimiter] = useState(props.delimiter ?? " ");
+    const [limit] = useState(props.limit ?? 5); // configurable in widget now
+    const [delimiter] = useState(props.delimiter ?? ",");
+    const [newTag, setNewTag] = useState(props.NewTag ?? ""); // new entity attribute for managing newTag value
     const [tagCount, setTagCount] = useState(0);
     const [tagCountDisplay, setTagCountDisplay] = useState("");
 
-    // listen for changes to the masterTagsList attribute and update tags
     useEffect(() => {
         if (mounted) {
-            console.debug("Updating tags with masterTagsList:", props.masterTagsList.value);
             updateAllTags(props.masterTagsList.value);
         }
-        return () => {
-            console.debug("Cleaning up tags update effect");
-        };
-        // adding 'mounted' dependency to fix initial rendering bug that occurred when moving wdiget outside of sandbox.
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        return () => {};
     }, [props.masterTagsList, mounted]);
 
-    // set mounted when render() is first called
     useEffect(() => {
-        console.debug("WorkflowContainer mounted");
         setMounted(true);
         return () => {};
     }, []);
 
-    // update state with new tag and call onChangeAction if set
     const saveTag = (index, oldValue, newValue) => {
-        console.debug("calling saveTag");
         const fullTagsArray = props.masterTagsList.value.split(delimiter).filter(i => i);
         fullTagsArray.splice(index, 1, newValue);
         const updatedMasterTagsList = fullTagsArray.join(delimiter);
-        console.debug("Data is changing in WorkflowContainer - saveTag:", oldValue, "=>", newValue);
         props.masterTagsList.setValue(updatedMasterTagsList);
         if (props.onChangeAction) {
-            props.onChangeAction.execute();
+            props.onChangeAction.execute(); // call onChange microflow
         }
         return true;
     };
 
-    // remove tag from list and update state
     const deleteTag = (index, value) => {
-        console.debug("calling deleteTag", tagsArray);
         const fullTagsArray = props.masterTagsList.value.split(delimiter).filter(i => i);
-        console.debug("fullTagsArray before", fullTagsArray);
         fullTagsArray.splice(index, 1);
-        console.debug("fullTagsArray after", fullTagsArray);
         const updatedMasterTagsList = fullTagsArray.join(delimiter);
         props.masterTagsList.setValue(updatedMasterTagsList);
         if (props.onChangeAction) {
-            props.onChangeAction.execute();
+            props.onChangeAction.execute(); // call onChange microflow
         }
         updateAllTags(updatedMasterTagsList);
     };
 
-    // update state with new masterTagsList and rebuild tag components
     const updateAllTags = value => {
-        console.debug("calling updateAllTags");
         if (value !== undefined) {
             const tagCount = value.split(delimiter).filter(i => i).length;
             var tagCountDisplay = tagCount - limit;
-            if (tagCount > 99) {
+            if (tagCount - limit > 99) {
                 tagCountDisplay = "99+";
             }
             const tagsArraySliced = value
@@ -90,36 +75,67 @@ export function WorkflowContainer(props) {
                     deleteTag={deleteTag.bind(this)}
                 />
             ));
-            console.debug("Data is changing in WorkflowContainer - updateAllTags:", "New value:", value);
             setMasterTagsList(value);
-            console.debug("setMasterTagsList", value);
             setTagsArray(tagsArraySliced);
-            console.debug("setTagsArray", tagsArraySliced);
             setTagComponents(newTagComponents);
-            console.debug("setTagComponents", newTagComponents);
             setTagCount(tagCount);
             setTagCountDisplay(tagCountDisplay);
         }
     };
 
-    // add a new tag to the front of the tags list
     const addTag = () => {
-        console.debug("calling addTag");
-        onTagAddAction.execute(); //this was broken so I refactored it out to a mf
+        // set the value of newTag attribute
+        props.newTag.setValue(newTag);
+
+        if (props.onTagAddAction) {
+            props.onTagAddAction.execute(); // call onAdd microflow
+        }
+        setNewTag(""); // clear the input field after adding the tag
+    };
+
+    const handleNewTagChange = event => {
+        const inputValue = event.target.value;
+        setNewTag(inputValue);
+        console.log("New Tag Value:", inputValue);
+    };
+
+    const inputRef = useRef(null);
+
+    const handleBlur = () => {
+        // delay on clearing input value to allow user to click the + icon for add
+        setTimeout(() => {
+            if (!inputRef.current.contains(document.activeElement)) {
+                // clear input value if click is not on the input
+                setNewTag(""); // clear  input field when it loses focus
+            }
+        }, 200); // clear-delay value
     };
 
     return (
         <div className="ev-workflow-container">
-            <button className="ev-workflow-admin-button ev-workflow-admin-button-add" onClick={addTag.bind(this)}>
-                Workflow: Add<i className="mdi mdi-plus"></i>
+            <button className="ev-workflow-admin-button ev-workflow-admin-button-add">
+                <input
+                    type="text"
+                    placeholder="add a tag"
+                    value={newTag}
+                    onChange={handleNewTagChange}
+                    onKeyDown={e => {
+                        if (e.key === "Enter") {
+                            addTag();
+                        }
+                    }}
+                    onBlur={handleBlur}
+                    ref={inputRef}
+                />
+                <span onClick={addTag.bind(this)}>
+                    <i className="mdi mdi-plus"></i>
+                </span>
             </button>
-            {tagComponents.map(tag => {
-                return tag;
-            })}
+            {tagComponents.map(tag => tag)}
             {tagCount - limit > 0 ? (
                 <button
                     className="ev-workflow-admin-button ev-workflow-admin-button-view-all"
-                    onClick={() => onClickMoreAction.execute()}
+                    onClick={() => onClickMoreAction.execute()} // call tag overflow microflow
                 >
                     <i className="mdi mdi-open-in-new"></i>
                     {tagCountDisplay} more
